f57a38aa895c7771348eed5ab7cf6ee2eb5ef9cf
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-12-04 17:58:53 -0600

    begin implementing game sessions

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
-var session = require('express-session')
+var session = require('express-session');
+var uid = require('rand-token').uid;
 var path = require('path');
 
 var port = process.env.PORT || 8080;
@@ -9,6 +10,7 @@ var publicPath = path.resolve(__dirname, '../public');
 
 var users = {};
 var randomMatchers = [];
+var games = {};
 
 // Sessions
 app.use(session({
@@ -27,17 +29,29 @@ app.use(function (req, res, next) {
   next(); 
 });
 
+app.get('/game', function(req, res) {
+  res.sendFile(path.join(publicPath, 'game/index.html'));
+});
+app.ws('/game', function(ws, req) {
+  ws.send(JSON.stringify(req));
+});
+
 app.ws('/match', function(ws, req) {
   if (randomMatchers.length > 0) {  // Players available
-    users[randomMatchers[0]].onMatch(req.sessionID);
-    ws.send(randomMatchers.shift());
+    var gameID = uid(4);
+    var matchedPlayer = randomMatchers.shift();
+    games[gameID] = {
+      players: [matchedPlayer, req.sessionID]
+    };
+
+    users[matchedPlayer].onMatch(gameID);
+    ws.send(gameID);
   } else {                          // No players available
     users[req.sessionID].onMatch = function (partnerID) {
       ws.send(partnerID);
     };
     randomMatchers.push(req.sessionID);
   }
-  console.log(randomMatchers);
 });
 
 // Serve static files
