e76b3837ac4e8ef18bbd6022da4c8b65721ec0a7
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-12-04 10:37:29 -0600

    get basic ws set up

--- group_2_finalproject/server/index.js
+++ group_2_finalproject/server/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
+var path = require('path');
+
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname, '../index.html'));
+});
 
 app.ws('/echo', function(ws, req) {
   ws.on('message', function(msg) {
     ws.send(msg);
   });
 });
+
+app.listen(3000);
+console.log('listening on port 3000');
