0714c3fbed23623e1406b44000184a67b53a289d
everydaydaniel <everydaydaniel1994@gmail.com>
2017-12-07 16:57:42 -0600

    did stars works

--- socketsTut/public/galaga.js
+++ socketsTut/public/galaga.js
@@ -1,6 +1,6 @@
 // porting the game from processing to p5.js
 const gamePieces = []
-
+const stars = []
 function SpaceShip(isMine) {
   // initialize the object
   this.isMine = isMine;
@@ -12,11 +12,17 @@ function SpaceShip(isMine) {
     this.ypos = height - 2 * this.ht;
     this.reloadTimer = 11;
     this.reloadTimerMax = 10;
+    this.health = 1000;
+    this.SpaceShipImg = goodShip;
+
+
   // if it is an enemy ship.
   } else {
     this.ypos = this.ht;
     this.reloadTimer = 91;
     this.reloadTimerMax = random(60,120);
+    this.health = 400;
+    this.SpaceShipImg = badShip;
   }
 
   this.speed = 5;
@@ -25,17 +31,54 @@ function SpaceShip(isMine) {
   this.destX = random(width);
   this.destY = random(0,height/2);
 
-  if (isMine){
-    this.health = 1000;
-    this.SpaceShipImg = goodShip;
-
-  }
-  else{
-    this.health = 400;
-  }
-
-
   // add to the gamePieces array.
   gamePieces.push(this);
+
+}
+
+
+function backStars(){
+  // used to create the background stars.
+  this.xpos = random(width);
+  this.ypos = random(height);
+  this.wdth = random(2,5);
+  this.ht = this.wdth;
+  this.speed = this.wdth;
+  this.rotation = 0
+  stars.push(this);
+
+  // update the stars
+  this. update = function(){
+    this.ypos += this.speed;
+    if (this.ypos > height){
+      this.ypos = -this.ht;
+      this.xpos = random(width);
+    }
+  }
+
+  // display stars
+  this.display = function() {
+    fill(255, map(this.wdth, 2, 5, 255/2, 255));
+    rect(this.xpos, this.ypos, this.wdth, this.ht);
+    stroke(255);
+    push();
+      translate(this.xpos+2,this.ypos+2);
+      rotate(-this.rotation);
+      line(0,5,0,-5);
+      line(-5,0,5,0);
+    pop();
+    push();
+      translate(this.xpos+2,this.ypos+2);
+      rotate(-this.rotation);
+      line(0,5,0,-5);
+      line(-5,0,5,0);
+    pop();
+    this.rotaion += .06;
+    noStroke();
+
+  }
+
+
+
 
 }
--- socketsTut/public/sketch.js
+++ socketsTut/public/sketch.js
@@ -3,45 +3,31 @@ var t;
 var f;
 var goodShip, badShip;
 function preload(){
-	// preload the ship images 
+	// preload the ship images
 	goodShip = loadImage("assets/Enemy_Ship.png");
 	badShip = loadImage("assets/Galaga_Ship.png");
 }
 
 function setup() {
-	createCanvas(600, 600);
-	background(51);
-	socket = io.connect('http://localhost:3000');
-	socket.on("mouse",newDrawing);
+	createCanvas(800, 800);
 	t = new SpaceShip(true);
 	f = new SpaceShip(false);
 	console.log(t,f);
-	console.log(gamePieces);
-}
 
-function newDrawing(data){
-	noStroke();
-	fill(40,12,70);
-	ellipse(data.x,data.y,60,60);
-
-}
-
-function mouseDragged(){
-	console.log("sending: ",mouseX,mouseY);
-	fill(255);
-	noStroke();
-	ellipse(mouseX, mouseY,60,60)
-	// create a message top send
-	var data = {
-		x: mouseX,
-		y: mouseY
+	// create stars.
+	for (var i = 0; i < 90; i++) {
+		new backStars();
 	}
-
-	socket.emit("mouse",data);
-
+	console.log(gamePieces);
+	console.log(stars);
 }
 
 
 function draw() {
+	background(0);
+	for (var i = 0; i < stars.length; i++) {
+		stars[i].display();
+		stars[i].update();
 
+	}
 }
