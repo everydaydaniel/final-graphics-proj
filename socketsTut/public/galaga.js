7a75377f1f4793eac72ab8e3eb19251f48a945fb
everydaydaniel <everydaydaniel1994@gmail.com>
2017-12-10 20:52:27 -0600

    added most functionality

--- socketsTut/public/galaga.js
+++ socketsTut/public/galaga.js
@@ -1,9 +1,13 @@
 // porting the game from processing to p5.js
-const gamePieces = []
-const stars = []
+
+var enemies = [];
+var gamePieces = [];
+var stars = [];
+var lasers = [];
 
 function SpaceShip(isMine) {
   // initialize the object
+  this.dead = false;
   this.isMine = isMine;
   this.width = 30;
   this.ht = 30;
@@ -12,18 +16,24 @@ function SpaceShip(isMine) {
   if (this.isMine) {
     this.ypos = height - 2 * this.ht;
     this.reloadTimer = 11;
-    this.reloadTimerMax = 10;
-    this.health = 1000;
+    this.reloadTimerMax = 15;
+    this.health = 300;
     this.SpaceShipImg = goodShip;
+    this.direction = 8;
+    // add to the gamePieces array.
+    gamePieces.push(this);
 
 
     // if it is an enemy ship.
   } else {
     this.ypos = this.ht;
     this.reloadTimer = 91;
-    this.reloadTimerMax = random(60, 120);
-    this.health = 400;
+    this.reloadTimerMax = random(60, 120) - difficulty;
+    this.health = 150 * difficulty;
     this.SpaceShipImg = badShip;
+    this.direction = -8;
+    // add to the enemy array
+    enemies.push(this);
   }
 
   this.speed = 5;
@@ -32,28 +42,147 @@ function SpaceShip(isMine) {
   this.destX = random(width);
   this.destY = random(0, height / 2);
 
-  // add to the gamePieces array.
-  gamePieces.push(this);
 
   // update position
   this.update = function() {
-
+    // if (this.health < 350){
+    //   console.log("its working");
+    //   console.log(this.health);
+    // }
+    // check if it is a players game Piece
     if (this.isMine) {
-      function keyPressed() {
-        if (keyCode == RIGHT_ARROW) {
-          this.xpos += speed;
-          console.log(this.xpos);
-        } else if (keyCode == LEFT_ARROW) {
-          this.xpos -= speed;
-          console.log(this.xpos);
+      if (keyIsPressed === true) {
+
+        if (keyCode == RIGHT_ARROW && this.xpos < width - 60) {
+
+          this.xpos += this.speed;
+          // console.log(this.xpos);
+
+        } else if (keyCode == LEFT_ARROW && this.xpos > 0) {
+          this.xpos -= this.speed;
+          // console.log(this.xpos);
         }
       }
+      // handle shooting here 32 == spacebar
+      if (mouseIsPressed || keyIsPressed && keyCode == 32) {
+        this.shoot();
+      }
+    }
+    // enemy movement
+    else {
+      this.shoot();
+      this.xpos = lerp(this.xpos, this.destX, 0.01);
+      this.ypos = lerp(this.ypos, this.destY, 0.01);
+
+      this.dist = sqrt(pow(this.xpos - this.destX, 2) + pow(this.ypos - this.destY, 2));
+
+      if (this.dist < 0.1) {
+        this.destX = random(width);
+        this.destY = random(0, height / 2);
+      }
+
+    }
+    this.reloadTimer += 1;
+    if (this.health <= 0) {
+      this.dead = true;
+    }
+  }
+  // shoot the bullet boi
+  this.shoot = function() {
+    if (this.reloadTimer > this.reloadTimerMax) {
+      // new laser LaserBullet
+      new LaserBullet(this.xpos, this.ypos, this.direction, this.isMine);
+      this.reloadTimer = 0;
+      // console.log("Shoot");
     }
 
   }
 
+  // display the ship
+  this.display = function() {
+    image(this.SpaceShipImg, this.xpos, this.ypos, 60, 60);
+
+    if (this.isMine){
+      
+      rect(30, 30, map(this.health, 0, 100, 0, this.wdth), 3);
+    }
+    else{
+      rect(this.xpos, this.ypos - 3, map(this.health, 0, 100, 0, this.wdth), 3);
+    }
+  }
+
 }
 
+// laser bullet class
+function LaserBullet(xpos, ypos, direction, isMine) {
+  // assign what to loop through
+  this.shotTarget = false;
+  this.targets = gamePieces;
+  this.ypos = ypos + 50;
+  if (isMine) {
+    this.targets = enemies;
+    this.ypos -= 20
+  }
+
+
+  this.xpos = xpos + 30;
+  this.ypos = ypos;
+  this.direction = -1 * direction;
+  this.wdth = 3;
+  this.ht = 20;
+  this.isLaserBullet = true;
+  lasers.push(this);
+  // console.log(direction);
+  // Update function
+  this.update = function() {
+    this.ypos += this.direction;
+    if (this.shotTarget == false) {
+      // update bullet position
+
+
+      // loop throught the targets
+      for (var i = 0; i < this.targets.length; i++) {
+        let shotX = false;
+        let shotY = false;
+        let gp = this.targets[i];
+
+        if ((this.xpos > gp.xpos + 10 && this.xpos < gp.xpos + gp.wdth + 10) ||
+          (this.xpos + this.wdth > gp.xpos + 10 &&
+            this.xpos + this.wdth < gp.xpos + gp.xpos + 10)) {
+          shotX = true;
+        }
+        if ((this.ypos > gp.ypos && this.ypos < gp.ypos + gp.ht) || (this.ypos + this.ht > gp.ypos && this.ypos + this.ht < gp.ypos + gp.ypos)) {
+          shotY = true;
+        }
+        if (shotX && shotY) {
+          gp.health -= 50;
+          this.shotTarget = true;
+
+        }
+      }
+      // if (this.ypos < -this.ht || this.ypos > height){
+      //   let i = lasers.indexOf(this);
+      //   if (i != -1){
+      //     lasers = lasers.splice(i,1);
+      //   }
+      // }
+    }
+  }
+
+  this.display = function() {
+    fill(0, 255, 0);
+    rect(this.xpos, this.ypos, this.wdth, this.ht);
+  }
+
+  this.removeLasers = function() {
+    if (lasers.length > 70) {
+
+      lasers = lasers.slice(-15);
+    }
+  }
+
+
+}
 
 function backStars() {
   // used to create the background stars.
@@ -95,8 +224,27 @@ function backStars() {
     noStroke();
 
   }
+}
 
+function allDead(arr) {
+  for (var i = 0; i < arr.length; i++) {
 
+    if (arr[i].dead == false) {
+      return false;
+    }
+    return true;
+  }
+}
+
+function newLevel() {
+  enemies = [];
+  difficulty += .15;
+  for (var i = 0; i < 4; i++) {
+    new SpaceShip(false);
+  }
+  for (var i = 0; i < gamePieces.length; i++) {
+    gamePieces[i].health = 300;
+  }
 
 
 }
--- socketsTut/public/sketch.js
+++ socketsTut/public/sketch.js
@@ -2,32 +2,62 @@ var socket;
 var t;
 var f;
 var goodShip, badShip;
-function preload(){
-	// preload the ship images
-	goodShip = loadImage("assets/Enemy_Ship.png");
-	badShip = loadImage("assets/Galaga_Ship.png");
+var difficulty = 1;
+
+function preload() {
+  // preload the ship images
+  badShip = loadImage("assets/Enemy_Ship.png");
+  goodShip = loadImage("assets/Galaga_Ship.png");
 }
 
 function setup() {
-	createCanvas(800, 800);
-	t = new SpaceShip(true);
-	f = new SpaceShip(false);
-	console.log(t,f);
+  createCanvas(600, 600);
+  t = new SpaceShip(true);
+  for (var i = 0; i < 1; i++) {
+    new SpaceShip(false);
+  }
+  f = new SpaceShip(false);
+  console.log(t, f);
 
-	// create stars.
-	for (var i = 0; i < 90; i++) {
-		new backStars();
-	}
-	console.log(gamePieces);
-	console.log(stars);
+  // create stars.
+  for (var i = 0; i < 90; i++) {
+    new backStars();
+  }
+  console.log(gamePieces);
+  console.log(stars);
 }
 
 
 function draw() {
-	background(0);
-	for (var i = 0; i < stars.length; i++) {
-		stars[i].display();
-		stars[i].update();
+  background(0);
+  // generate stars
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].display();
+    stars[i].update();
+  }
 
-	}
+  for (var i = 0; i < gamePieces.length; i++) {
+    if (gamePieces[i].dead == false) {
+      gamePieces[i].update();
+      gamePieces[i].display();
+    }
+  }
+
+	if (allDead(enemies)){
+    console.log(difficulty)
+		newLevel();
+	 }
+  for (var i = 0; i < enemies.length; i++) {
+    if (enemies[i].dead == false) {
+      enemies[i].update();
+      enemies[i].display();
+    }
+  }
+
+  for (var i = 0; i < lasers.length; i++) {
+    lasers[i].update();
+    lasers[i].display();
+    lasers[i].removeLasers();
+  }
+
 }
